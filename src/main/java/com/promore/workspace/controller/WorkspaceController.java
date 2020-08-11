064aee06765c8d37710bffd4bedfa44f3bc0ec2c
이형은 <lhe22@hanmail.net>
2020-08-11 10:33:42 +0900

    no message

--- src/main/java/com/promore/workspace/controller/WorkspaceController.java
+++ src/main/java/com/promore/workspace/controller/WorkspaceController.java
@@ -1,4 +1,4 @@
-package com.promore.workspace;
+package com.promore.workspace.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.promore.aop.HAspect;
+
 @Controller
 public class WorkspaceController {
 	
 	@RequestMapping(value = "/workspace/workspace.do", method = RequestMethod.GET)
 	public ModelAndView workspace(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("Ok");
+		HAspect.logger.info(HAspect.logMsg+"workspaceOk");
 		
 		ModelAndView mav = new ModelAndView("/workspace/workspace");
 		return mav;
