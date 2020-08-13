9a60cdf70ce2d4e94a8899921a00365cf52761d0
sohyunkim <blumen1229@naver.com>
2020-08-13 16:47:03 +0900

    customerController 수정

--- src/main/java/com/promore/customer/controller/CustomerController.java
+++ src/main/java/com/promore/customer/controller/CustomerController.java
@@ -3,35 +3,65 @@ package com.promore.customer.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.promore.aop.HAspect;
+import com.promore.customer.dto.CustomerDto;
+import com.promore.customer.service.CustomerService;
+
 @Controller
 public class CustomerController {
-		
-	@RequestMapping(value = "/customer/inquire.do", method = RequestMethod.GET)
-	public ModelAndView customerInquire(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("Ok");
-		
-		ModelAndView mav = new ModelAndView("customer/inquire");
-		return mav;
-	}
+	@Autowired
+	private CustomerService customerService;
 	
-	@RequestMapping(value = "/customer/notice.do", method = RequestMethod.GET)
-	public ModelAndView customerNotice(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("Ok");
+	public CustomerController() {}
 
-		ModelAndView mav = new ModelAndView("customer/notice");
+	public CustomerController(CustomerService customerService) {
+		this.customerService = customerService;
+	}
+
+	public void setCustomerServicee(CustomerService customerService) {
+		this.customerService = customerService;
+	}
+	
+	@RequestMapping(value = "/customer/inquireList.do", method = RequestMethod.GET)
+	public ModelAndView customerBoardList(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("request", request);
+		
+		customerService.customerBoardList(mav);
+		return mav;		
+	}
+	
+	@RequestMapping(value = "/customer/inquireWrite.do", method = RequestMethod.POST)
+	public ModelAndView customerBoardWrite(HttpServletRequest request, HttpServletResponse response, CustomerDto customerDto) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("request", request);
+		mav.addObject("customerDto", customerDto);
+		
+		customerService.customerBoardWriteOk(mav);
 		return mav;
 	}
 	
-	@RequestMapping(value = "/customer/modify.do", method = RequestMethod.GET)
-	public ModelAndView customerModify(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("Ok");
+	@RequestMapping(value = "/customer/inquireDelete.do", method = RequestMethod.GET)
+	public ModelAndView noticeBoardDelete(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("request", request);
 		
-		ModelAndView mav = new ModelAndView("customer/modify");
-		return mav;
+		customerService.customerBoardDeleteOk(mav);
+			
+		return mav;		
+	}
+	
+	@RequestMapping(value = "/customer/noticeList.do", method = RequestMethod.GET)
+	public ModelAndView noticeBoardList(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("request", request);
+		
+		return mav;		
 	}
 }
